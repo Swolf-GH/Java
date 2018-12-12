1e97c3a985bb7fa3051c32e7b38fab2ccfe60f36
Swolf-GH <swolf.xv@gmail.com>
2018-12-12 22:29:51 +0800

    Note01
    20181212

---
+++ CoreJavaVolume/Chapter03/src/Annotation.java
@@ -0,0 +1,20 @@
+/**
+ * Java 中的各种注释
+ * @author kk
+ *
+ */
+public class Annotation {
+	public static void main(String[] args) {
+		System.out.println("Hello wrold"); // 单行注释   多么经典的一句话呀
+		/*
+		 * 这
+		 * 是
+		 * 一
+		 * 个
+		 * 多
+		 * 行
+		 * 注
+		 * 释
+		 */
+	}
+}
---
+++ CoreJavaVolume/Chapter03/src/FirstSample.java
@@ -0,0 +1,6 @@
+
+public class FirstSample {
+	public static void main(String[] arg) {
+		System.out.println("we will no use 'Hello world'");
+	}
+}
